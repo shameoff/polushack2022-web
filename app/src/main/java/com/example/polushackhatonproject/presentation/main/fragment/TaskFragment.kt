7633c8a266414acb58154d3cf0925019170b72c6
Gulevskii Ivan <ithirteengw@gmail.com>
2022-10-15 19:13:43 +0700

    1) connected to YandexMapsApi 2) some changes in design

--- app/src/main/java/com/example/polushackhatonproject/presentation/main/fragment/TaskFragment.kt
+++ app/src/main/java/com/example/polushackhatonproject/presentation/main/fragment/TaskFragment.kt
@@ -6,14 +6,45 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.polushackhatonproject.R
+import com.example.polushackhatonproject.databinding.FragmentTaskBinding
+import com.yandex.mapkit.Animation
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
 
 class TaskFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task, container, false)
+
+    companion object {
+        const val MAPS_API_KEY = "f73c83ab-4ec2-487a-a28f-3ac53d948347"
     }
+
+    private lateinit var binding: FragmentTaskBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        val mainView = inflater.inflate(R.layout.fragment_task, container, false)
+        binding = FragmentTaskBinding.bind(mainView)
+        binding.mapView.map.move(
+            CameraPosition(Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 0f),
+            null
+        )
+
+        return mainView
+    }
+
+    override fun onStop() {
+        binding.mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        binding.mapView.onStart()
+    }
+
 
 }
