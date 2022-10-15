7b9a09e2c0fa759f891b9f1f4f20e34c0ae12bdd
Egor Shamov <eshamov030316@gmail.com>
2022-10-16 05:30:42 +0700

    Починил ДБ, контейнеры работают в связке

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './routes/index.js';
+import cors from 'cors'
 
 const app = express();
 app.use(express.json());
--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -3,14 +3,12 @@ import pkg from 'pg';
 
 dotenv.config({ path: '../.env' })
 
-
 const { Pool } = pkg;
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
 });
 
 export default pool;
