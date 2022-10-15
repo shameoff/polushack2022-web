9bf882b9e13601696bced937bde52b0a231f6b3b
vanya vorobev <ivanvorobev14042004@mail.ru>
2022-10-15 08:50:33 +0700

    add icons for tab layout

--- app/src/main/java/com/example/polushackhatonproject/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/polushackhatonproject/presentation/main/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.polushackhatonproject.presentation.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.polushackhatonproject.R
+import com.example.polushackhatonproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
