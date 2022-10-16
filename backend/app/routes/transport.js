3263df4da95ef69b5c51bd0f6cfeeca47baf8e03
Egor Shamov <eshamov030316@gmail.com>
2022-10-16 07:23:21 +0700

    Сговнокодил апишку для транспорта. Не проверял

--- backend/app/routes/transport.js
+++ backend/app/routes/transport.js
@@ -3,11 +3,25 @@ import TransportController from '../controller/transport.controller.js';
 
 const transportRouter = Router();
 transportRouter.get(
-    '/transport/', (req, res) => {
-                TransportController.getAllTransports(req, res);
-              }
+  '/transport/', (req, res) => {
+    TransportController.getTransport(req, res);
+  }
 )
-
+transportRouter.post(
+  '/transport/', (req, res) => {
+    TransportController.addTransport(req,res);  
+  }
+)
+transportRouter.put(
+  '/transport/:id', (req, res) => {
+    TransportController.editTransport(req,res);  
+  }
+)
+transportRouter.delete(
+  '/transport/:id', (req, res) => {
+    TransportController.addTransport(req,res);  
+  }
+)
 // transportRouter.get('/transport/', TransportController.getAllTransports)
 
 export default transportRouter;
